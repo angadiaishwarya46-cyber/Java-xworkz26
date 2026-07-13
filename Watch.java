bf188b45aa8eeb4509aeb2e741f1c2fb88dc9c61
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-13 12:47:26 -0400

    java code

---
+++ Watch.java
@@ -0,0 +1,93 @@
+class Watch {
+
+   public static void getBrand() {
+      System.out.println("The watch brand is FOSSIL");
+   }
+
+   public static void getPrice() {
+      System.out.println("The watch price is Rs. 8,995");
+   }
+
+   public static void getWatchType() {
+      System.out.println("The watch type is Thick Straps, Metallic");
+   }
+
+   public static void getDisplayType() {
+      System.out.println("The display type is Analog");
+   }
+
+   public static void getIdealFor() {
+      System.out.println("The watch is ideal for Men");
+   }
+
+   public static void getWaterResistant() {
+      System.out.println("The watch is Water Resistant: Yes");
+   }
+
+   public static void getMechanism() {
+      System.out.println("The watch mechanism is Quartz");
+   }
+
+   public static void getStyleCode() {
+      System.out.println("The watch style code is CH2601");
+   }
+
+   public static void getSeries() {
+      System.out.println("The watch series is DECKER");
+   }
+
+   public static void getOccasion() {
+      System.out.println("The occasion is Casual");
+   }
+
+   public static void getMovement() {
+      System.out.println("The movement is Quartz");
+   }
+
+   public static void getPackOf() {
+      System.out.println("Pack of: 1");
+   }
+
+   public static void getSalesPackage() {
+      System.out.println("Sales Package: 1");
+   }
+
+   public static void getShockResistance() {
+      System.out.println("Shock Resistant: No");
+   }
+
+   public static void getScratchResistance() {
+      System.out.println("Scratch Resistant: No");
+   }
+
+   public static void getStrapColor() {
+      System.out.println("The strap color is Black");
+   }
+
+   public static void getNetQuantity() {
+      System.out.println("Net Quantity: 1");
+   }
+
+   public static void main(String[] arg) {
+
+      getBrand();
+      getPrice();
+      getWatchType();
+      getDisplayType();
+      getIdealFor();
+      getWaterResistant();
+      getMechanism();
+      getStyleCode();
+      getSeries();
+      getOccasion();
+      getMovement();
+      getPackOf();
+      getSalesPackage();
+      getShockResistance();
+      getScratchResistance();
+      getStrapColor();
+      getNetQuantity();
+
+   }
+
+}
