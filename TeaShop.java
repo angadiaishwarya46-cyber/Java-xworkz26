6ab83b89bf68c298df72a6a862226963ff203ef3
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-13 14:00:54 -0400

    arrayexample

---
+++ TeaShop.java
@@ -0,0 +1,14 @@
+class TeaShop {
+
+  //starting point
+  public static void main(String []ref){
+    // code 
+	//Statements ;
+	 
+	    String teaNames[] = {"Masala Tea","Elaichi Tea","Lemon Tea", "Green Tea"};
+		System.out.println(teaNames[0]);
+		System.out.println(teaNames[1]);
+		System.out.println(teaNames[2]);
+		}
+		
+}
