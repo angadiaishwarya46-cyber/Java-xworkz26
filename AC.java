84f1c0e5a74a2ff8d73aba882c9c287c9d062319
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-13 13:51:40 -0400

    conditionalstatements

---
+++ AC.java
@@ -0,0 +1,86 @@
+class AC { 
+    static boolean isConnected; 
+    static int maxTemperature = 7; 
+    static int currentTemperature; 
+    static int minTemperature; 
+
+    public static void onOroff(){ 
+        if (isConnected == false){ 
+            isConnected = true; 
+            System.out.println("AC is connected..."); 
+        } 
+		else if (isConnected == true){ 
+            isConnected = false; 
+            System.out.println("AC is dis-connected..."); 
+        } 
+        return; 
+    } 
+
+    public static void increaseTemperature(){ 
+        if(isConnected == true){ 
+            if(currentTemperature < maxTemperature){ 
+                currentTemperature = currentTemperature + 1; 
+                System.out.println("The currentTemperature is " + currentTemperature); 
+            } 
+			else if (currentTemperature == maxTemperature) { 
+                System.out.println("Max Temperature reached..."); 
+            } 
+        } 
+		else if (isConnected == false) { 
+            System.out.println("Turn On the AC first..."); 
+        } 
+    } 
+
+    public static void decreaseTemperature(){ 
+        if(isConnected == true){ 
+            if(currentTemperature > minTemperature){ 
+                currentTemperature = currentTemperature - 1; 
+                System.out.println("The currentTemperature is " + currentTemperature); 
+            } 
+			else if (currentTemperature == minTemperature) { 
+                System.out.println("Max Temperature reached..."); 
+            } 
+        } 
+		else if (isConnected == false) { 
+            System.out.println("Turn On the AC first..."); 
+        } 
+    } 
+
+    public static void main(String[] args){ 
+        
+        System.out.println("main started"); 
+        System.out.println(isConnected); 
+        onOroff(); 
+        System.out.println(isConnected); 
+        onOroff(); 
+        System.out.println("main ended"); 
+
+        
+        System.out.println("main started"); 
+        System.out.println(isConnected); 
+        onOroff(); 
+        increaseTemperature(); 
+        increaseTemperature(); 
+        increaseTemperature(); 
+        increaseTemperature(); 
+        increaseTemperature(); 
+        increaseTemperature(); 
+        increaseTemperature(); 
+        onOroff(); 
+        System.out.println("main ended"); 
+
+        
+        System.out.println("main started"); 
+        System.out.println(isConnected); 
+        onOroff(); 
+        decreaseTemperature(); 
+        decreaseTemperature(); 
+        decreaseTemperature(); 
+        decreaseTemperature(); 
+        increaseTemperature(); 
+        decreaseTemperature(); 
+        decreaseTemperature(); 
+        onOroff(); 
+        System.out.println("main ended"); 
+    } 
+}
