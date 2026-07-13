fab36647c8c557d31d787779bf03e7247fd78249
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-13 12:41:09 -0400

    java code

---
+++ AirPurifier.java
@@ -0,0 +1,55 @@
+class AirPurifier {
+
+   public static void getBrand() {
+      System.out.println("The air purifier brand is Philips");
+   }
+
+   public static void getPrice() {
+      System.out.println("The air purifier price is Rs. 12,999");
+   }
+
+   public static void getCoverageArea() {
+      System.out.println("The coverage area is 400 sq.ft");
+   }
+
+   public static void getFilterType() {
+      System.out.println("The filter type is HEPA Filter");
+   }
+
+   public static void getColor() {
+      System.out.println("The color is White");
+   }
+
+   public static void getPowerConsumption() {
+      System.out.println("The power consumption is 50 Watts");
+   }
+
+   public static void getWarranty() {
+      System.out.println("The warranty is 2 Years");
+   }
+
+   public static void getDisplayType() {
+      System.out.println("The display type is LED Display");
+   }
+
+   public static void getSleepMode() {
+      System.out.println("Sleep Mode is available");
+   }
+
+   public static void getAirQualityIndicator() {
+      System.out.println("Air Quality Indicator is available");
+   }
+
+   public static void main(String[] arg) {
+      getBrand();
+      getPrice();
+      getCoverageArea();
+      getFilterType();
+      getColor();
+      getPowerConsumption();
+      getWarranty();
+      getDisplayType();
+      getSleepMode();
+      getAirQualityIndicator();
+   }
+}
