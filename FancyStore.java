8b627677091619afeb9ce0f26b25d3cffb315b4d
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-13 14:06:43 -0400

    arrayexample

---
+++ FancyStore.java
@@ -0,0 +1,27 @@
+class FancyStore {
+
+     public static void main(String []ref){
+	 
+	 String differentItems[] = {"Earings","Lipsticks","Bangles","Clutches","Bindis","Nailpolish","Mirrors","Notebooks"};
+     System.out.println(differentItems[0]);
+	 System.out.println(differentItems[1]);
+	 System.out.println(differentItems[2]);
+	 System.out.println(differentItems[3]);
+	 System.out.println(differentItems[4]);
+	 System.out.println(differentItems[5]);
+	 System.out.println(differentItems[6]);
+	 System.out.println(differentItems[7]);
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 }
+
+
+
+
+
+}
