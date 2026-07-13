5fdb1010eded5f4a4c254cec18d97df97b288b47
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-13 13:35:16 -0400

    conditionalstatements

---
+++ Dmart.java
@@ -0,0 +1,99 @@
+class Dmart {
+
+   public static void main(String[]arg){
+   
+   String groceries[]={"Bread","Milk","Rice","Eggs","Apples","Cheese"};
+   String clothes[]={"Zudio","Performax","Nike","Zara","Adidas","Puma"};
+   String sports[]={"Cricket Bats","Footballs","Excercise Mats","Basketballa","Tennis Balls","Swimming Gear"};
+   String kidsSection[]={"EducationalToys","SchoolStationary","KidsFootwear","SoftToys","Plastic Cars","Slime"};
+   String perfumes[]={"Fogg","Engage","Denver","Signature","Bella Vita","Renee"};
+   String shoes[]={"Bata","Sparx","Paragon","Campus","Flite","Inbul"};
+   String cosmetics[]={"Kajal","Lipstick","Sindoor","BB Cream","Nail Polish","Eyeliner"};
+   String snacks[]={"Chips","Cookies","Biscuits","Bingo","Mixtures","Cakes"};
+   
+   
+      System.out.println(groceries[0]);
+      System.out.println(groceries[1]);
+      System.out.println(groceries[2]);
+      System.out.println(groceries[3]);
+      System.out.println(groceries[4]);
+      System.out.println(groceries[5]);
+      
+      System.out.println(clothes[0]);
+      System.out.println(clothes[1]);
+      System.out.println(clothes[2]);
+      System.out.println(clothes[3]);
+      System.out.println(clothes[4]);
+      System.out.println(clothes[5]);
+     
+      System.out.println(sports[0]);
+      System.out.println(sports[1]);
+      System.out.println(sports[2]);
+      System.out.println(sports[3]);
+      System.out.println(sports[4]);
+      System.out.println(sports[5]);
+	  
+	  System.out.println(kidsSection[0]);
+      System.out.println(kidsSection[1]);
+      System.out.println(kidsSection[2]);
+      System.out.println(kidsSection[3]);
+      System.out.println(kidsSection[4]);
+      System.out.println(kidsSection[5]);
+      
+      System.out.println(perfumes[0]);
+      System.out.println(perfumes[1]);
+      System.out.println(perfumes[2]);
+      System.out.println(perfumes[3]);
+      System.out.println(perfumes[4]);
+      System.out.println(perfumes[5]);
+     
+      System.out.println(shoes[0]);
+      System.out.println(shoes[1]);
+      System.out.println(shoes[2]);
+      System.out.println(shoes[3]);
+      System.out.println(shoes[4]);
+      System.out.println(shoes[5]);
+	  
+	  System.out.println(cosmetics[0]);
+      System.out.println(cosmetics[1]);
+      System.out.println(cosmetics[2]);
+      System.out.println(cosmetics[3]);
+      System.out.println(cosmetics[4]);
+      System.out.println(cosmetics[5]);
+     
+      System.out.println(snacks[0]);
+      System.out.println(snacks[1]);
+      System.out.println(snacks[2]);
+      System.out.println(snacks[3]);
+      System.out.println(snacks[4]);
+      System.out.println(snacks[5]);
+     
+   
+   
+   
+   
+     
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   }
+
+
+
+}
+
