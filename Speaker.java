490fa2a58a9093edd302931b2c931b5bc3551b7a
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-13 13:54:41 -0400

    conditionalstatements

---
+++ Speaker.java
@@ -0,0 +1,30 @@
+class Speaker {
+
+   public static void getBrand() {
+      System.out.println("The speaker brand is JBL");
+   }
+
+   public static void getPrice() {
+      System.out.println("The speaker price is Rs. 8,999");
+   }
+
+   public static void getConnectivity() {
+      System.out.println("The connectivity type is Bluetooth");
+   }
+
+   public static void getBatteryLife() {
+      System.out.println("The battery life is 12 Hours");
+   }
+
+   public static void getWaterResistant() {
+      System.out.println("Water Resistant : Yes");
+   }
+
+   public static void main(String[] arg) {
+      getBrand();
+      getPrice();
+      getConnectivity();
+      getBatteryLife();
+      getWaterResistant();
+   }
+}
