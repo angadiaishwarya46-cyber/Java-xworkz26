28e915818e9a318d54e159c85f58330ffff37ceb
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-13 14:04:41 -0400

    arrayexample

---
+++ Salon.java
@@ -0,0 +1,14 @@
+class Salon {
+
+   public static void main(String []arg){
+   
+   
+     String differentHaircuts[] = {"U Shape","V Shape","Step Cut","Layer Cut"};
+	 System.out.println(differentHaircuts[0]);
+	 System.out.println(differentHaircuts[1]);
+	 System.out.println(differentHaircuts[2]);
+	 System.out.println(differentHaircuts[3]);
+	 
+	 }
+	 
+}
