f9a8b73275dfde7801833ea4f18fc9255481183f
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-14 22:34:31 -0400

    methodsExample

---
+++ Refrigerator.java
@@ -0,0 +1,25 @@
+class Refrigerator {
+
+   public static void getBrand() {
+      System.out.println("The refrigerator brand is LG");
+   }
+
+   public static void getCapacity() {
+      System.out.println("The capacity is 260 Litres");
+   }
+
+   public static void getPrice() {
+      System.out.println("The refrigerator price is Rs. 32,999");
+   }
+
+   public static void getDoorType() {
+      System.out.println("The door type is Double Door");
+   }
+
+   public static void main(String[] arg) {
+      getBrand();
+      getCapacity();
+      getPrice();
+      getDoorType();
+   }
+}
