de52a5aef98863c5f0ab3ab3213bb3fffb53b00e
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-14 22:36:08 -0400

    methodsExample

---
+++ Earbuds.java
@@ -0,0 +1,25 @@
+class Earbuds {
+
+   public static void getBrand() {
+      System.out.println("The earbuds brand is OnePlus");
+   }
+
+   public static void getModel() {
+      System.out.println("The earbuds model is Nord Buds 3R");
+   }
+
+   public static void getPrice() {
+      System.out.println("The earbuds price is Rs. 1,999");
+   }
+
+   public static void getConnectivity() {
+      System.out.println("The connectivity type is Bluetooth");
+   }
+
+   public static void main(String[] arg) {
+      getBrand();
+      getModel();
+      getPrice();
+      getConnectivity();
+   }
+}
