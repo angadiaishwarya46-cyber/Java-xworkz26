e216e1c0ea9f58f203f39c038e56ff151b7a6338
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-14 22:30:21 -0400

    methodsExample

---
+++ Printer.java
@@ -0,0 +1,25 @@
+class Printer {
+
+   public static void getBrand() {
+      System.out.println("The printer brand is Canon");
+   }
+
+   public static void getModel() {
+      System.out.println("The printer model is MG2570S");
+   }
+
+   public static void getPrice() {
+      System.out.println("The printer price is Rs. 3,599");
+   }
+
+   public static void getType() {
+      System.out.println("The printer type is Inkjet");
+   }
+
+   public static void main(String[] arg) {
+      getBrand();
+      getModel();
+      getPrice();
+      getType();
+   }
+}
