035515ffc0a0194fb3b6ca21a3214ac45fd5e508
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-14 22:28:46 -0400

    methodsExample

---
+++ Mobile.java
@@ -0,0 +1,30 @@
+class Mobile {
+
+   public static void getBrand() {
+      System.out.println("The mobile brand is Samsung");
+   }
+
+   public static void getModel() {
+      System.out.println("The mobile model is Galaxy S25");
+   }
+
+   public static void getPrice() {
+      System.out.println("The mobile price is Rs. 84,999");
+   }
+
+   public static void getStorage() {
+      System.out.println("The storage is 256 GB");
+   }
+
+   public static void getBattery() {
+      System.out.println("The battery capacity is 5000 mAh");
+   }
+
+   public static void main(String[] arg) {
+      getBrand();
+      getModel();
+      getPrice();
+      getStorage();
+      getBattery();
+   }
+}
