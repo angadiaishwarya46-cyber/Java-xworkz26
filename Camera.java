1496970ee083827010f80d7b6b44f4af3ccac837
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-14 22:40:58 -0400

    methodsExample

---
+++ Camera.java
@@ -0,0 +1,30 @@
+class Camera {
+
+   public static void getBrand() {
+      System.out.println("The camera brand is Sony");
+   }
+
+   public static void getPrice() {
+      System.out.println("The camera price is Rs. 47,990");
+   }
+
+   public static void getCameraType() {
+      System.out.println("The camera type is Digital Camera");
+   }
+
+   public static void getMegaPixels() {
+      System.out.println("The megapixels are 20.1 MP");
+   }
+
+   public static void getZoom() {
+      System.out.println("The zoom is 8X Optical Zoom");
+   }
+
+   public static void main(String[] arg) {
+      getBrand();
+      getPrice();
+      getCameraType();
+      getMegaPixels();
+      getZoom();
+   }
+}
