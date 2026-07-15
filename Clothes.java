a5bae28b86dd16f56ab8e3d33c532b9955e84d7d
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-14 22:43:44 -0400

    methodsExample

---
+++ Clothes.java
@@ -0,0 +1,56 @@
+class Clothes {
+
+   public static void getBrand() {
+      System.out.println("The clothes brand is Allen Solly");
+   }
+
+   public static void getPrice() {
+      System.out.println("The clothes price is Rs. 1,499");
+   }
+
+   public static void getType() {
+      System.out.println("The type is Formal Shirt");
+   }
+
+   public static void getColor() {
+      System.out.println("The color is Blue");
+   }
+
+   public static void getSize() {
+      System.out.println("The size is Medium");
+   }
+
+   public static void getMaterial() {
+      System.out.println("The material is Cotton");
+   }
+
+   public static void getSleeveType() {
+      System.out.println("The sleeve type is Full Sleeve");
+   }
+
+   public static void getFit() {
+      System.out.println("The fit is Regular Fit");
+   }
+
+   public static void getWashCare() {
+      System.out.println("Machine Wash is recommended");
+   }
+
+   public static void getCountryOfOrigin() {
+      System.out.println("The country of origin is India");
+   }
+
+   public static void main(String[] arg) {
+
+      getBrand();
+      getPrice();
+      getType();
+      getColor();
+      getSize();
+      getMaterial();
+      getSleeveType();
+      getFit();
+      getWashCare();
+      getCountryOfOrigin();
+   }
+}
