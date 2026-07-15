962331c1e028ecd2111ce103d8ef6592fc9c5e91
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-14 22:19:42 -0400

    methodsExample

---
+++ Calculator.java
@@ -0,0 +1,51 @@
+class Calculator {
+
+ 
+   public static void add(int first, int second) {
+            int total = first+second;
+			  System.out.println(total);
+			  
+   }
+			  
+   public static void multiply(int first, int second) {
+             int total = first*second;
+			   System.out.println(total);
+   
+   }
+   
+   public static void divide(int first, int second) {
+             int total = first/second;
+			   System.out.println(total);
+   
+   }
+   
+   public static void subtract(int first, int second) {
+             int total = first-second;
+			   System.out.println(total);
+   
+   
+   }
+   
+   public static void mod(int first, int second) {
+             int total = first%second;
+			   System.out.println(total);
+   
+   }
+
+
+  public static void main(String []arg) {
+    
+	add(78 , 89);
+	multiply(78 ,89);
+	divide(50 , 4);
+	subtract(100 , 67);
+	mod(50 , 35);
+  
+  
+  }
+  
+  
+}
+  
+ 
+
