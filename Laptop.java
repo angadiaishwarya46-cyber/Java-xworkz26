a8f5a5d7547cf774dddbbe7d1c19407b49640495
angadiaishwarya46-cyber <angadiaishwarya46@gmail.com>
2026-07-14 22:23:59 -0400

    methodsExample

---
+++ Laptop.java
@@ -0,0 +1,48 @@
+class Laptop {
+
+   public static void getBrand() {
+      System.out.println("The laptop brand is HP");
+   }
+
+   public static void getModel() {
+      System.out.println("The laptop model is Victus");
+   }
+
+   public static void getPrice() {
+      System.out.println("The laptop price is Rs. 79,990");
+   }
+
+   public static void getProcessor() {
+      System.out.println("The processor is Intel Core i5");
+   }
+
+   public static void getRam() {
+      System.out.println("The RAM is 16 GB");
+   }
+
+   public static void getStorage() {
+      System.out.println("The storage is 512 GB SSD");
+   }
+
+   public static void getDisplaySize() {
+      System.out.println("The display size is 15.6 inch");
+   }
+
+   public static void getOperatingSystem() {
+      System.out.println("The operating system is Windows 11");
+   }
+
+   public static void main(String[] arg) {
+
+      getBrand();
+      getModel();
+      getPrice();
+      getProcessor();
+      getRam();
+      getStorage();
+      getDisplaySize();
+      getOperatingSystem();
+
+   }
+
+}
